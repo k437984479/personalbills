714c347177441eee098198d13c2d79148106a7c6
k437984479 <437984479@qq.com>
2020-09-26 19:30:31 +0800

    生成项目

---
+++ src/main/java/com/kk/bill/PersonalbillsApplication.java
@@ -0,0 +1,12 @@
+package com.kk.bill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalbillsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalbillsApplication.class, args);
+    }
+}
---
+++ src/main/java/com/kk/bill/controller/billController.java
@@ -0,0 +1,12 @@
+package com.kk.bill.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author jinkk
+ * @2020/9/26 19:12
+ * 文件说明：
+ */
+@Controller
+public class billController {
+}
